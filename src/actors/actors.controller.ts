9e97f4be49b293f00c4866179f4b51b10c6307d8
Huy Huỳnh Nguyễn Minh <dev.hnmhuy@gmail.com>
2024-10-06 18:49:22 +0700

    Add validation and documentation

--- src/actors/actors.controller.ts
+++ src/actors/actors.controller.ts
@@ -3,32 +3,41 @@ import { ActorsService } from './actors.service';
 import { CreateActorDto } from './dto/create-actor.dto';
 import { UpdateActorDto } from './dto/update-actor.dto';
 import { Actor } from 'src/database/entities/Actor';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('actors')
 @Controller('api/actors')
 export class ActorsController {
   constructor(private readonly actorsService: ActorsService) {}
 
   @Post()
+  @ApiOperation({summary: 'Create a new actor'})
+  @ApiBody({type: CreateActorDto})
   create(@Body() createActorDto: CreateActorDto) {
     return this.actorsService.create(createActorDto);
   }
 
   @Get()
+  @ApiOperation({summary: 'Get all actors'})
   async findAll(): Promise<Actor[]> {
     return await this.actorsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({summary: "Get a specific actor by id"})
   async findOne(@Param('id') id: string): Promise<Actor> {
     return await this.actorsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiBody({type: UpdateActorDto})
+  @ApiOperation({summary: 'Update an exisiting actor by id'})
   update(@Param('id') id: string, @Body() updateActorDto: UpdateActorDto) {
     return this.actorsService.update(+id, updateActorDto);
   }
 
   @Delete(':id')
+  @ApiOperation({summary: 'Delete an actor by id'})
   remove(@Param('id') id: string) {
     return this.actorsService.remove(+id);
   }
--- src/actors/dto/create-actor.dto.ts
+++ src/actors/dto/create-actor.dto.ts
@@ -1,4 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class CreateActorDto {
+    @IsNotEmpty()
+    @IsString()
+    @ApiProperty({description: "Actor's firstname"})
     firstName: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @ApiProperty({description: "Actor's lastname"})
     lastName: string;
 }
